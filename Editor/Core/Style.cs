a4c3a32c10a56306adfa5f55ca060305980d5e65
Martônio Júnior <martoniojunior@hotmail.com>
2022-08-11 15:54:13 -0300

    refactor(Editor.Core): Present and Style classes

---
+++ Editor/Core/Style.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace MartonioJunior.Trinkets.Editor
+{
+    public static class Style
+    {
+        #region Static Properties
+        public static GUIStyle New => new GUIStyle();
+        #endregion
+        #region Static Methods
+        public static GUIStyle BasedOn(GUIStyle reference)
+        {
+            return new GUIStyle(reference);
+        }
+        #endregion
+    }
+}
