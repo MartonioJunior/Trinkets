b0b1de34313ee48f31081e329dc318f7e510f26f
Martônio Júnior <martoniojunior@hotmail.com>
2022-08-19 19:50:49 -0300

    feat(CurrencyWallet): New method With added
    Allows for multiple change operations at once in a builder-like coding style.

--- Runtime/Currency/ICurrencyWallet.cs
+++ Runtime/Currency/ICurrencyWallet.cs
@@ -20,5 +20,22 @@ namespace MartonioJunior.Trinkets.Currencies
         */
 	    void Reset(ICurrency currency);
         #endregion
-    }   
+    }
+
+    public static partial class ICurrencyWalletExtensions
+    {
+        /**
+        <returns>The wallet where the resources were added in.</returns>
+        <remarks>Useful for making multiple additions at once.</remarks>
+        <inheritdoc cref="ICurrencyWallet.Change(ICurrency, int)"/>
+        */
+        public static ICurrencyWallet With(this ICurrencyWallet self, ICurrency currency, int delta)
+        {
+            if (currency != null) {
+                self.Change(currency, delta);
+            }
+
+            return self;
+        }
+    }
 }
