dc44d921c988e1ff7193f072f0e72de86a3c8693
Martônio Júnior <martoniojunior@hotmail.com>
2022-10-07 15:44:05 -0300

    chore(Collectable): Add missing files from CollectableCategory

--- Runtime/Collectables/ICollectableCategory.cs
+++ Runtime/Collectables/ICollectableCategory.cs
@@ -3,7 +3,7 @@ namespace MartonioJunior.Trinkets.Collectables
     /**
     <summary>Interface used to describe a category of collectables.</summary>
     */
-    public interface ICollectableCategory: IResource, IResourceCategory, IResourceManager<ICollectable>
+    public interface ICollectableCategory: IResourceTag
     {
         
     }
