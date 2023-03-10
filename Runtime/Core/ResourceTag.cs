15e7d6f62a0a52585a3f51e64ff73b96e9f7681e
Martônio Júnior <martoniojunior@hotmail.com>
2023-03-10 10:27:56 -0300

    change(ResourceTag): IResourceTag implemented via fields

--- Runtime/Core/ResourceTag.cs
+++ Runtime/Core/ResourceTag.cs
@@ -7,28 +7,15 @@ namespace MartonioJunior.Trinkets
     */
     public class ResourceTag: ScriptableObject, IResourceTag
     {
-        #region Variables
-        /**
-        <inheritdoc cref="IRepresentable.Image" />
-        */
-        [SerializeField] string displayName;
-        /**
-        <inheritdoc cref="IRepresentable.Name" />
-        */
-        [SerializeField] Sprite displayIcon;
-        #endregion
         #region IResource Implementation
-        public string Name {
-            get => displayName;
-            set => displayName = value;
-        }
         /**
-        <inheritdoc cref="IRepresentable.Image" />
+        <inheritdoc />
         */
-        public Sprite Image {
-            get => displayIcon;
-            set => displayIcon = value;
-        }
+        [field: SerializeField] public string Name {get; set;}
+        /**
+        <inheritdoc />
+        */
+        [field: SerializeField] public Sprite Image {get; set;}
         #endregion
     }
 }
